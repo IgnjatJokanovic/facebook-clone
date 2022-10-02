b5e8511ea40cd95c705f2685128244a67448f7ae
IgnjatJokanovic <jokanovic.ignjat@gmail.com>
2022-10-02 19:28:24 +0200

    Initial setup

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="img/favicon-32x32.png" sizes="32x32">
+    <link rel="stylesheet" rel="preload"  href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+        integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>FoxBook</title>
+
+    @viteReactRefresh
+    @vite('resources/js/app.jsx')
+    @vite('resources/sass/app.scss')
+    @vite('resources/sass/app.css')
+</head>
+
+<body>
+
+    <div id="app"></div>
+</body>
+
+</html>
